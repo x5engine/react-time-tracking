f6d0e76ca323494de51661f665ffc7fcb32f70b9
X5 Engine <12363020+x5engine@users.noreply.github.com>
2020-08-31 14:47:41 +0100

    update app name

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,7 +40,7 @@ class Header extends React.Component {
           <FontIcon className="material-icons" style={{
             color: appBar.textColor
           }}>alarm_on</FontIcon>
-          <ToolbarTitle text="Vivid Trace" style={{
+          <ToolbarTitle text="X5 Time" style={{
             color: appBar.textColor
           }}/>
         </ToolbarGroup>
